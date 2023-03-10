38af1260a6d87a6d0805d08b54a98bce676b8ae4
jhon <jhon.bocanegra26@gmail.com>
2023-03-10 00:01:49 -0500

    agregados assets y creacion del hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          background: '#3d4957',
+          primary: '#90adbb',
+          secondary: '#a3b4ff',
+          tertiary: '#3e3e70',
+      },
+      backgroundImage: {
+        'hero': "url('/public/assets/imgs/san-francisco.jpg')"
+      }
+    }
   },
   plugins: [],
 }
